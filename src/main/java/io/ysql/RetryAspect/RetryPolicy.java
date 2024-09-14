57252fed5ad3c9d1c793c1855c2e75b3d8a9f641
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2024-09-14 07:26:20 +0530

    [update]: retry codes
    Signed-off-by: Srinivasa Vasu <srinivasan.surprise@gmail.com>

--- src/main/java/io/ysql/RetryAspect/RetryPolicy.java
+++ src/main/java/io/ysql/RetryAspect/RetryPolicy.java
@@ -3,6 +3,9 @@ package io.ysql.RetryAspect;
 import java.sql.SQLException;
 import java.sql.SQLRecoverableException;
 import java.sql.SQLTransientConnectionException;
+import java.util.Map;
+import java.util.Optional;
+import java.util.function.Predicate;
 
 import jakarta.annotation.PostConstruct;
 
@@ -20,33 +23,41 @@ public class RetryPolicy extends ExceptionClassifierRetryPolicy {
 	// 08006 - connection issues (error sending data back);"kill -9" failures, socket timeouts
 	// 57P01 - broken pool conn (invalidated connections because of node-failure, node-restart, etc.);"kill -15" failures
 	// XX000 - RPC failures (could be intermittent)
-	private final String SQL_STATE = "^(40001)|(40P01)|(57P01)|(08006)|(XX000)";
+	private final String SQL_STATE = "^(40001)|(40P01)|(57P01)|(08006)";
 
 	// oom-killer or a process crash in the middle of a tx
 	private final String SQL_MSG = "^(connection is closed)";
+
+	// XX000 shouldn't be re-tried. Retry it only when the state and msg matches
+	private final Map<String, String> errorCodes = Map.of("XX000", "schema version mismatch");
 
 	private final org.springframework.retry.RetryPolicy sp = new SimpleRetryPolicy(5);
 
 	private final org.springframework.retry.RetryPolicy np = new NeverRetryPolicy();
 
+	private final Predicate<SQLException> sqlStatePredicate = exception -> Optional.ofNullable(exception.getSQLState())
+			.filter(state -> (state.matches(SQL_STATE) || (errorCodes.containsKey(exception.getSQLState()) && (
+					Optional.ofNullable(exception.getMessage())
+							.filter(msg -> msg.contains(errorCodes.get(exception.getSQLState()))).isPresent()))))
+			.isPresent();
+
+	private final Predicate<SQLException> sqlMsgPredicate = exception ->
+			Optional.ofNullable(exception.getMessage()).map(String::toLowerCase).filter(msg -> msg.matches(SQL_MSG))
+					.isPresent();
+
+	// SQLTransientConnectionException: (08001)|(08003)
+	// - intermittent issues because of a backend failure like connection refused
+	// - hikari connection time out
+	// - 08001, 08003 - connection does not exist (pool connection timeout)
+	private final Predicate<Throwable> exceptionPredicate = exception -> (exception instanceof SQLRecoverableException || exception instanceof SQLTransientConnectionException || exception instanceof TransientDataAccessException);
+
 	@PostConstruct
 	public void init() {
 		this.setExceptionClassifier(cause -> {
 			do {
-				// SQLTransientConnectionException: (08001)|(08003)
-				// - intermittent issues because of a backend failure like connection refused
-				// - hikari connection time out
-				// - 08001, 08003 - connection does not exist (pool connection timeout)
-				if (cause instanceof SQLRecoverableException || cause instanceof SQLTransientConnectionException ||
-						cause instanceof TransientDataAccessException) {
+				if (exceptionPredicate.test(cause) || (cause instanceof SQLException exception && (sqlStatePredicate.or(sqlMsgPredicate)
+						.test(exception)))) {
 					return sp;
-				}
-				else if (cause instanceof SQLException exception) {
-					if ((exception.getSQLState() != null && exception.getSQLState().matches(SQL_STATE))
-							|| (exception.getMessage() != null
-							&& exception.getMessage().toLowerCase().matches(SQL_MSG))) {
-						return sp;
-					}
 				}
 				cause = cause.getCause();
 			}
